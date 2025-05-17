88aace69d4569486ca3bb12c7938d8155412cb58
QuesoParmesano <pecazon03@gmail.com>
2025-05-17 15:05:35 -0300

    Funcionalidad "eliminarPrenda" terminada

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const path = require('path');
 const exp = express()   
 const port = 3000
 
+console.clear()
+
 exp.use(express.static(path.join(__dirname)));
 exp.use(express.urlencoded({ extended: true }))
 exp.use(express.json())
 
 exp.post('/eliminar-prenda', async (req, res) => {
     const idPrenda = req.body['idPrenda']
-    const result = await eliminarPrenda(idPrenda)
-    console.log(result)
+    let result = await eliminarPrenda(idPrenda)
+    // res -> devuelve a la aplicacion la confirmacion de que sucedio con la accion
 })
-
 
 exp.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
